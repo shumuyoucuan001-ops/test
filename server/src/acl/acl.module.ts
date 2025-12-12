ef3692e071193b09d0970ae8fb8192e3eb16d2ca
shumuyoucuan001-ops <shumuyoucuan001@gmail.com>
2025-12-12 14:38:25 +0800

    在 AclModule 中导出 AclService,在 DingTalkModule 中导入 AclModule

--- server/src/acl/acl.module.ts
+++ server/src/acl/acl.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DingTalkModule } from '../dingtalk/dingtalk.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AclController } from './acl.controller';
 import { AclService } from './acl.service';
 
 @Module({
-  imports: [PrismaModule, DingTalkModule],
+  imports: [PrismaModule, forwardRef(() => DingTalkModule)], // 使用forwardRef解决循环依赖
   controllers: [AclController],
   providers: [AclService],
+  exports: [AclService], // 导出AclService供其他模块使用
 })
 export class AclModule { }
 
--- server/src/dingtalk/dingtalk.module.ts
+++ server/src/dingtalk/dingtalk.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
+import { AclModule } from '../acl/acl.module';
 import { DingTalkController } from './dingtalk.controller';
 import { DingTalkService } from './dingtalk.service';
 
@@ -7,6 +8,7 @@ import { DingTalkService } from './dingtalk.service';
  * 类似Java的@Configuration配置类
  */
 @Module({
+    imports: [forwardRef(() => AclModule)], // 使用forwardRef解决循环依赖
     providers: [DingTalkService], // 类似@Bean注解
     controllers: [DingTalkController], // 类似@RestController
     exports: [DingTalkService], // 导出服务供其他模块使用
